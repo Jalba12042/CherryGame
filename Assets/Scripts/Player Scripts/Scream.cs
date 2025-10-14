9d904286c025591805e92c62411e6ac7ab59b22c
riatmax <riatmax@gmail.com>
2025-10-13 22:02:04 -0400

    added a working scream button (SEE DESCRIPTION FOR MORE))
    Added a button that picks a random soundclip to play and a random pitch

---
+++ Assets/Scripts/Player Scripts/Scream.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Scream : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> screamSFX;
+    
+    private PlayerMovement player;
+    private AudioSource aSource;
+    private Gamepad gp;
+
+    private void Start()
+    {
+        player = GetComponent<PlayerMovement>();
+        aSource = GetComponent<AudioSource>();
+        gp = player.assignedGamepad;
+    }
+
+    private void Update()
+    {
+        if (gp.buttonEast.wasPressedThisFrame)
+        {
+            int rand = Random.Range(0, screamSFX.Count);
+            float randPitch = Random.Range(0f, 2f);
+            aSource.pitch = randPitch;
+            aSource.PlayOneShot(screamSFX[rand]);
+        }
+    }
+}
