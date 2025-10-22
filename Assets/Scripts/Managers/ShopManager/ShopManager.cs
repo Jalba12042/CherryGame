654749db13e06e1615ea05ae60ed00f9e05583af
Vicman8 <Victor2044usa@gmail.com>
2025-10-22 18:28:06 -0400

    Added controller input onto the shop section
    This also includes button highlighting with colors for players (ex P1 is highlighted blue), also added an array of buttons that holds the votes

--- Assets/Scripts/Managers/ShopManager/ShopManager.cs
+++ Assets/Scripts/Managers/ShopManager/ShopManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class ShopManager : MonoBehaviour
 {
@@ -13,16 +15,93 @@ public class ShopManager : MonoBehaviour
     [SerializeField] private TMP_Text[] buttonDescs;
     [SerializeField] private TMP_Text timerText;
     private float timer;
+    public Button[] shopButtons;
+
+    [Header("Highlight Images (per player per button)")]
+    public Image[,] playerHighlights = new Image[4, 4]; // [playerIndex, buttonIndex]
+
+    private int[] currentIndexes = new int[4];
+    private bool[] canMove = new bool[4];
+    private int[] buttonVotes;
+
+    private Color[] playerColors = { Color.blue, Color.red, Color.green, Color.yellow };
 
     private void Start()
     {
         setupButtons();
+
+        int playerCount = Mathf.Min(GameManager.Instance.playerCount, Gamepad.all.Count);
+        buttonVotes = new int[shopButtons.Length];
+
+        for (int i = 0; i < 4; i++)
+        {
+            currentIndexes[i] = 0;
+            canMove[i] = true;
+        }
+
+        SetupHighlights();
+        HighlightButtons();
+
         StartCoroutine(StartShopTimer());
     }
 
     private void Update()
     {
         timerText.text = $"{shopTimerDurationInSecs - (int)timer}";
+        HandleControllerInput();
+    }
+
+    private void HandleControllerInput()
+    {
+        int playerCount = Mathf.Min(GameManager.Instance.playerCount, Gamepad.all.Count);
+
+        for (int i = 0; i < playerCount; i++)
+        {
+            var gamepad = Gamepad.all[i];
+            Vector2 move = gamepad.leftStick.ReadValue();
+
+            if (canMove[i])
+            {
+                if (move.y > 0.5f)
+                {
+                    currentIndexes[i] = Mathf.Max(0, currentIndexes[i] - 1);
+                    canMove[i] = false;
+                    HighlightButtons();
+                }
+                else if (move.y < -0.5f)
+                {
+                    currentIndexes[i] = Mathf.Min(shopButtons.Length - 1, currentIndexes[i] + 1);
+                    canMove[i] = false;
+                    HighlightButtons();
+                }
+            }
+
+            if (Mathf.Abs(move.y) < 0.2f)
+                canMove[i] = true;
+
+            if (gamepad.buttonSouth.wasPressedThisFrame)
+            {
+                int chosenButton = currentIndexes[i];
+                buttonVotes[chosenButton]++;
+                Debug.Log($"Player {i + 1} (Color {playerColors[i]}) voted for button {chosenButton + 1}. Total votes: {buttonVotes[chosenButton]}");
+            }
+        }
+    }
+
+    private void HighlightButtons()
+    {
+        int playerCount = Mathf.Min(GameManager.Instance.playerCount, Gamepad.all.Count);
+
+        for (int p = 0; p < playerCount; p++)
+        {
+            for (int b = 0; b < shopButtons.Length; b++)
+            {
+                if (playerHighlights[p, b] != null)
+                {
+                    playerHighlights[p, b].enabled = (b == currentIndexes[p]);
+                }
+            }
+        }
     }
 
     // Shop Timer
@@ -35,6 +114,12 @@ public class ShopManager : MonoBehaviour
             yield return null;
         }
         timer = shopTimerDurationInSecs;
+
+        for (int i = 0; i < buttonVotes.Length; i++)
+        {
+            Debug.Log($"Button {i + 1} got {buttonVotes[i]} votes");
+        }
+
         RoundManager.Instance.switchRoundScene();
     }
 
@@ -81,6 +166,33 @@ public class ShopManager : MonoBehaviour
         {
             buttonTexts[i].text = "SOLD OUT";
             buttonDescs[i].text = "";
+        }
+    }
+
+    private void SetupHighlights()
+    {
+        // Auto-create small highlight images under each button for each player
+        int playerCount = Mathf.Min(GameManager.Instance.playerCount, 4);
+
+        for (int p = 0; p < playerCount; p++)
+        {
+            for (int b = 0; b < shopButtons.Length; b++)
+            {
+                GameObject highlightObj = new GameObject($"Player{p + 1}_Highlight_Button{b + 1}");
+                highlightObj.transform.SetParent(shopButtons[b].transform, false);
+
+                Image img = highlightObj.AddComponent<Image>();
+                img.color = playerColors[p];
+                img.enabled = false;
+
+                // position/scale highlight (slightly smaller than button)
+                RectTransform rt = highlightObj.GetComponent<RectTransform>();
+                rt.anchorMin = new Vector2(0.05f, 0.05f);
+                rt.anchorMax = new Vector2(0.95f, 0.95f);
+                rt.offsetMin = rt.offsetMax = Vector2.zero;
+
+                playerHighlights[p, b] = img;
+            }
         }
     }
 }
