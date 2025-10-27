6710dd5301636d16ea599a1c323e2f970fe17a24
Vicman8 <Victor2044usa@gmail.com>
2025-10-27 15:09:23 -0400

    Updated powerups to be picked up using RT

--- Assets/Scripts/Level Scripts/Powerups/Powerup.cs
+++ Assets/Scripts/Level Scripts/Powerups/Powerup.cs
@@ -1,14 +1,50 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Powerup : MonoBehaviour
 {
     protected Playermovement pc;
+    private bool playerInRange = false;
+    private GameObject playerObj;
     protected virtual void powerUpEffect()
     {
         Debug.Log("powerup");
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            playerObj = other.gameObject;
+            pc = playerObj.GetComponent<Playermovement>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            playerObj = null;
+            pc = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (playerInRange && Gamepad.current != null)
+        {
+            // RT is the right trigger, which goes from 0.0 to 1.0 when pressed
+            if (Gamepad.current.rightTrigger.wasPressedThisFrame)
+            {
+                powerUpEffect();
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    /*private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
@@ -17,5 +53,5 @@ public class Powerup : MonoBehaviour
             Destroy(gameObject);
         }
             
-    }
+    }*/
 }
