6d903f42e36aaf170aeca6f9b15ebc737e681007
riatmax <riatmax@gmail.com>
2025-10-28 21:24:55 -0400

    updated powerup script to be more robust, also added duration

--- Assets/Scripts/Level Scripts/Powerups/JumpTestPowerup.cs
+++ Assets/Scripts/Level Scripts/Powerups/JumpTestPowerup.cs
@@ -2,8 +2,28 @@ using UnityEngine;
 
 public class JumpTestPowerup : Powerup
 {
+    [SerializeField] private float jumpMultiplier;
+    private float originalJumpForce;
+    private bool activated = false; // flag to make sure we dont infinitely increase the jump
     protected override void powerUpEffect()
     {
-        pc.jumpForce += 2f;
+        base.powerUpEffect();
+        originalJumpForce = pc.jumpForce;
+    }
+
+    private void Update()
+    {
+        if (pc != null)
+        {
+            if (active && !activated)
+            {
+                pc.jumpForce *= jumpMultiplier;
+                activated = true;
+            }
+            else if (!active)
+            {
+                pc.jumpForce = originalJumpForce;
+            }
+        }
     }
 }
--- Assets/Scripts/Level Scripts/Powerups/Powerup.cs
+++ Assets/Scripts/Level Scripts/Powerups/Powerup.cs
@@ -1,11 +1,32 @@
+using System.Collections;
 using UnityEngine;
 
 public class Powerup : MonoBehaviour
 {
     protected Playermovement pc;
-    protected virtual void powerUpEffect()
+    protected bool active = false; // is the powerup currently affecting the player?
+
+    [SerializeField] protected float duration;
+    [SerializeField] protected string puName;
+    protected IEnumerator startTimer()
     {
-        Debug.Log("powerup");
+        if (pc == null)
+        {
+            yield break;
+        }
+
+        // start powerup effects
+        powerUpEffect();
+        active = true;
+
+        // make it look invisible - Victor: make the object drop out of the player's hand here as well
+        GetComponent<Collider>().enabled = false;
+        GetComponent<MeshRenderer>().enabled = false;
+
+        yield return new WaitForSeconds(duration);
+
+        active = false;
+        Destroy(gameObject);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -13,9 +34,12 @@ public class Powerup : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             pc = collision.gameObject.GetComponent<Playermovement>();
-            powerUpEffect();
-            Destroy(gameObject);
-        }
-            
+            StartCoroutine(startTimer());
+        }    
+    }
+
+    protected virtual void powerUpEffect()
+    {
+        Debug.Log($"powerup: {puName}");
     }
 }
