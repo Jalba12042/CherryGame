389f4818a265932d31a88555965bc9dabda6c340
riatmax <riatmax@gmail.com>
2025-10-28 22:13:49 -0400

    fixed small issue where crate would spawn 2 powerups

--- Assets/Scripts/Level Scripts/Powerups/Crate.cs
+++ Assets/Scripts/Level Scripts/Powerups/Crate.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class Crate : MonoBehaviour
 {
+    private bool hasSpawned = false;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
-            GetComponent<MeshRenderer>().enabled = false;
-            GetComponent<Collider>().enabled = false;
+            if (hasSpawned) return;
+            else
+            {
+                hasSpawned = true;
+                GetComponent<MeshRenderer>().enabled = false;
+                GetComponent<Collider>().enabled = false;
 
-            Instantiate(RoundManager.Instance.powerUpsInRotation[Random.Range(0, RoundManager.Instance.powerUpsInRotation.Count)], transform.position, Quaternion.identity);
-            Destroy(gameObject);
+                Instantiate(RoundManager.Instance.powerUpsInRotation[Random.Range(0, RoundManager.Instance.powerUpsInRotation.Count)], transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
         }
     }
 }
