fa8f4949e74ecf0e374a3c8d3cd76de6a8f9cbae
riatmax <riatmax@gmail.com>
2025-11-04 20:42:18 -0500

    started adding powerup ids to powerups to populate a current powerups active list on each player

--- Assets/Scripts/Level Scripts/Powerups/Powerup.cs
+++ Assets/Scripts/Level Scripts/Powerups/Powerup.cs
@@ -6,42 +6,72 @@ public class Powerup : MonoBehaviour
     [SerializeField] protected float duration;
     [SerializeField] protected string puName;
 
+    public int powerUpID; 
+
     protected Playermovement pc;
     protected GameObject playerModel;
+
+    private Coroutine activeTimer;
 
     public void Activate(Playermovement player)
     {
         pc = player;
         playerModel = player.gameObject;
-        StartCoroutine(StartTimer());
+
+        // If already active, restart it instead of starting a duplicate
+        if (pc.currPowerups[powerUpID])
+        {
+            // Restart the timer
+            if (activeTimer != null)
+                StopCoroutine(activeTimer);
+
+            activeTimer = StartCoroutine(StartTimer(true));
+        }
+        else
+        {
+            // Mark active and start timer fresh
+            pc.currPowerups[powerUpID] = true;
+            activeTimer = StartCoroutine(StartTimer(false));
+        }
     }
 
-    protected virtual IEnumerator StartTimer()
+    protected virtual IEnumerator StartTimer(bool reset)
     {
         if (pc == null) yield break;
 
-        // start effect
-        powerUpEffect();
+        if (!reset)
+        {
+            // start effect
+            powerUpEffect();
 
-        // visually hide object
-        GetComponent<Collider>().enabled = false;
-        GetComponent<MeshRenderer>().enabled = false;
+            // visually hide object
+            GetComponent<Collider>().enabled = false;
+            GetComponent<MeshRenderer>().enabled = false;
+        }
+        else
+        {
+            Debug.Log($"{puName} timer reset for {pc.name}");
+        }
 
-        yield return new WaitForSeconds(duration);
+        float timer = 0;
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
 
         powerUpEnd();
-        //Destroy(gameObject);
     }
-
-    //RoundManager.Instance.currRound.powerupsInPlay.Remove(gameObject);
 
     protected virtual void powerUpEffect()
     {
+        pc.currPowerups[powerUpID] = true;
         Debug.Log($"Powerup activated: {puName} for {pc.name}");
     }
 
     protected virtual void powerUpEnd()
     {
+        pc.currPowerups[powerUpID] = false;
         Debug.Log($"Powerup ended: {puName} for {pc.name}");
     }
 }
