a73cd3c201362a164c373bcfadf6f11c5369ac57
riatmax <riatmax@gmail.com>
2025-11-17 13:07:05 -0500

    fixing taser

--- Assets/Scripts/Level Scripts/Powerups/Taser.cs
+++ Assets/Scripts/Level Scripts/Powerups/Taser.cs
@@ -5,10 +5,14 @@ public class Taser : Powerup
     protected override void powerUpEffect()
     {
         base.powerUpEffect();
+        pe.isTase = true;
+
     }
 
     protected override void powerUpEnd()
     {
         base.powerUpEnd();
+        pe.isTase = false;
+
     }
 }
