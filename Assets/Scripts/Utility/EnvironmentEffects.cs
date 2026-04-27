bf5bd295fb346b63d2b628a3643d850cca8f0fd0
riatmax <riatmax@gmail.com>
2026-04-27 19:10:40 -0400

    changed game to first to 3 instead of 5 and applied effects to growth powerup outlined in Trello, also fixed issue where crates don't break

--- Assets/Scripts/Utility/EnvironmentEffects.cs
+++ Assets/Scripts/Utility/EnvironmentEffects.cs
@@ -14,6 +14,9 @@ public class EnvironmentEffects : MonoBehaviour
         {
             if (exception != null && rb == exception) continue;
 
+            PlayerEffects pe = rb.GetComponent<PlayerEffects>();
+            if (pe != null && pe.isBig) continue;
+
             GroundCheck gc = rb.GetComponent<GroundCheck>();
             if (gc != null && gc.isGrounded)
             {
